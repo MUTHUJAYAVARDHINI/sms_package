0233cc446457aad507e38cdca37c6612afcf2736
vardhini <muthujayavardhinis@gramosoft.in>
2025-02-27 14:45:06 +0530

    sms_package

--- lib/sms_package.dart
+++ lib/sms_package.dart
@@ -1,5 +1,4 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library;
+
+export 'package:sms_package/src/features/Login/LoginScreen.dart';
+export 'package:sms_package/src/features/Login/OTPScreen.dart';
